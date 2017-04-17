944618f010b6ddd0437761ad37b89e2a5c38bb2e
york <york@SindeMacBook-Pro.local>
2017-04-17 08:23:04 +0800

    部分转换程序

---
+++ src/parse.go
@@ -0,0 +1,210 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/bullyork/serviceGen/src/tool"
+)
+
+type schema struct {
+	syntax   int
+	imports  []interface{}
+	enums    []int
+	messages []message
+	options  map[string]interface{}
+	extends  []interface{}
+	pack     string
+}
+
+type tokensArray struct {
+	data []string
+}
+
+func onpackagename(tokens *tokensArray) string {
+	shift(tokens)
+	name := tokens.data[0]
+	shift(tokens)
+	if tokens.data[0] != ";" {
+		fmt.Println("Expected ; but found " + tokens.data[0])
+	}
+	shift(tokens)
+	return name
+}
+
+func onsyntaxversion(tokens *tokensArray) int {
+	shift(tokens)
+	if tokens.data[0] != "=" {
+		fmt.Println("Expected = but found " + tokens.data[0])
+	}
+	shift(tokens)
+	versionStr := tokens.data[0]
+	var version int
+	shift(tokens)
+	switch versionStr {
+	case `"proto2"`:
+		version = 2
+	case `"proto3"`:
+		version = 3
+	default:
+		fmt.Println("Expected protobuf syntax version but found " + versionStr)
+	}
+	if tokens.data[0] != ";" {
+		fmt.Println("Expected ; but found " + tokens.data[0])
+	}
+	shift(tokens)
+	return version
+}
+
+type message struct {
+	name     string
+	enums    []int
+	extends  []string
+	messages []string
+	fields   []string
+}
+
+type field struct {
+	name     string
+	typeArea string
+	tag      int
+	mapArea  map[string]string
+	required bool
+	repeated bool
+	options  map[string]interface{}
+}
+
+type messageBody struct {
+	enums      []int
+	messages   []string
+	fields     []string
+	extends    []string
+	extensions extensions
+}
+
+type extensions struct {
+	from int
+	to   int
+}
+
+func onfield(tokens *tokensArray) field {
+	var field field
+	for len(tokens.data) > 0 {
+		switch tokens.data[0] {
+		case "=":
+			shift(tokens)
+			if v, err := strconv.Atoi(shift(tokens)); err == nil {
+				field.tag = v
+			} else {
+				fmt.Println(err)
+			}
+		case "map":
+			field.typeArea = "map"
+			shift(tokens)
+			if tokens.data[0] != "<" {
+				fmt.Println(`Unexpected token in map type: ` + tokens.data[0])
+			}
+			shift(tokens)
+			field.mapArea["from"] = shift(tokens)
+			if tokens.data[0] != "," {
+				fmt.Println(`Unexpected token in map type: ` + tokens.data[0])
+			}
+			shift(tokens)
+			field.mapArea["to"] = shift(tokens)
+			if tokens.data[0] != ">" {
+				fmt.Println(`Unexpected token in map type: ` + tokens.data[0])
+			}
+			shift(tokens)
+			field.name = shift(tokens)
+		case "repeated", "required", "optional":
+			var t = shift(tokens)
+			field.required = (t == "required")
+			field.repeated = (t == "repeated")
+			field.typeArea = shift(tokens)
+			field.name = shift(tokens)
+		case "[":
+			field.options = onfieldoptions(tokens)
+		case ";":
+			if field.name == "" {
+				fmt.Println("Missing field name")
+			}
+			if field.typeArea == "" {
+				fmt.Println("Missing type in message field: " + field.name)
+			}
+			if field.tag == -1 {
+				fmt.Println("Missing tag number in message field: " + field.name)
+			}
+			shift(tokens)
+			return field
+		default:
+			fmt.Println("Unexpected token in message field: " + tokens.data[0])
+		}
+	}
+	fmt.Println("No ; found for message field")
+	return field
+}
+
+func onmessagebody(tokens *tokensArray) messageBody {
+	var body messageBody
+	for len(tokens.data) > 0 {
+		switch tokens.data[0] {
+		case "map", "repeated", "optional", "required":
+			append(body.fields, onfield(tokens))
+			break
+		}
+	}
+}
+
+func onmessage(tokens *tokensArray) message {
+	shift(tokens)
+	lvl := 1
+	var bodyTokens tokensArray
+	var msg message
+	msg.name = shift(tokens)
+	if tokens.data[0] != "{" {
+		fmt.Println(`Expected { but found '` + tokens.data[0])
+	}
+	shift(tokens)
+	for len(tokens.data) > 0 {
+		if tokens.data[0] == "{" {
+			lvl++
+		} else if tokens.data[0] == "}" {
+			lvl--
+		}
+		if lvl == 0 {
+			shift(tokens)
+			body = onmessagebody(*bodyTokens)
+			msg.enums = body.enums
+			msg.messages = body.messages
+			msg.fields = body.fields
+			msg.extends = body.extends
+			msg.extensions = body.extensions
+			return msg
+		}
+		bodyTokens.data = append(bodyTokens.data, shift(tokens))
+	}
+}
+
+func shift(tokens *tokensArray) string {
+	str := tokens.data[0]
+	tokens.data = tokens.data[1:]
+	return str
+}
+
+func main() {
+	var tokens tokensArray
+	tokens.data = tool.Token("../proto/express/common.proto")
+
+	var sch schema
+	firstline := true
+	for len(tokens.data) > 0 {
+		switch tokens.data[0] {
+		case "package":
+			sch.pack = onpackagename(&tokens)
+		case "syntax":
+			sch.syntax = onsyntaxversion(&tokens)
+		case "message":
+			append(sch.messages, onmessage(&tokens))
+		}
+	}
+}
---
+++ src/tool/tool.go
@@ -0,0 +1,138 @@
+package tool
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strings"
+)
+
+var inside = false
+
+// Map 方法
+func Map(vs []string, f func(string) string) []string {
+	vsm := make([]string, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
+}
+
+// Filter 方法
+func Filter(vs []string, f func(string) bool) []string {
+	vsf := make([]string, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
+	return vsf
+}
+
+// Substr 方法
+func Substr(str string, start int, end int) string {
+	rs := []rune(str)
+	length := len(rs)
+
+	if start < 0 || start > length {
+		fmt.Println("start is wrong")
+	}
+
+	if end < 0 || end > length {
+		fmt.Println("end is wrong")
+	}
+
+	return string(rs[start:end])
+}
+
+func trimCallback(s string) string {
+	return strings.TrimSpace(s)
+}
+
+func filterBoolean(s string) bool {
+	if s == "" {
+		return false
+	}
+	return true
+}
+
+func noComments(line string) string {
+	i := strings.Index(line, "//")
+	if i > -1 {
+		return Substr(line, 0, i)
+	}
+	return line
+}
+
+func noMultilineComments(line string) bool {
+	i := strings.Index(line, "/*")
+	j := strings.Index(line, "*/")
+	if i > -1 {
+		inside = true
+		return false
+	}
+	if j > -1 {
+		inside = false
+		return false
+	}
+	return !inside
+}
+
+func concatCopyPreAllocate(slices [][]string) []string {
+	var totalLen int
+	for _, s := range slices {
+		totalLen += len(s)
+	}
+	tmp := make([]string, totalLen)
+	var i int
+	for _, s := range slices {
+		i += copy(tmp[i:], s)
+	}
+	return tmp
+}
+
+// Token 方法
+func Token(path string) []string {
+	fileInfo, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Println("请检查读取路径是否错误！")
+	}
+	contents := string(fileInfo)
+	re, _ := regexp.Compile(`([;,{}\(\)=\:\[\]<>]|\/\*|\*\/)`)
+	contents = re.ReplaceAllString(contents, " $1 ")
+	contentsArray := strings.Split(contents, "\n")
+	contentSlice := contentsArray[:]
+	contentSlice = Map(contentSlice, trimCallback)
+	contentSlice = Filter(contentSlice, filterBoolean)
+	contentSlice = Map(contentSlice, noComments)
+	contentSlice = Filter(contentSlice, noMultilineComments)
+	contentSlice = Map(contentSlice, trimCallback)
+	contentSlice = Filter(contentSlice, filterBoolean)
+	contents = strings.Join(contentSlice, "\n")
+	contentSlice = regexp.MustCompile(`\s+|\n+`).Split(contents, -1)
+	for i, v := range contentSlice {
+		re1, _ := regexp.Compile(`^(\"|\')([^\'\"]*)$`)
+		flag1 := re1.MatchString(v)
+		if flag1 {
+			var j int
+			if count := strings.Count(v, ""); count == 1 {
+				j = i + 1
+			} else {
+				j = i
+			}
+			for ; j < len(contentSlice); j++ {
+				re2, _ := regexp.Compile(`^([^\'\"]*)(\"|\')$`)
+				flag2 := re2.MatchString(contentSlice[j])
+				if flag2 {
+					s1 := contentSlice[0:i]
+					s3 := contentSlice[(j + 1):]
+					temp := contentSlice[i:(j + 1)]
+					s2 := []string{strings.Join(temp, "")}
+					slices := [][]string{s1, s2, s3}
+					contentSlice = concatCopyPreAllocate(slices)
+				}
+			}
+		}
+	}
+	return contentSlice
+}
