17dd23b47eabb50df9d0e96d52f23ebadf7021a9
张雁峰 <zhangyanfeng@ezbuy.com>
2017-05-02 17:17:02 +0800

    使用viper解析yaml

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	viper.SetConfigType("yaml")   // or viper.SetConfigType("YAML")
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.AddConfigPath(".")      // optionally look for config in the working directory
+	err := viper.ReadInConfig()   // Find and read the config file
+	if err != nil {               // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	fmt.Println(viper.Get("name"))
 }
